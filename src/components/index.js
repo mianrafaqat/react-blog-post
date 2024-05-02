68d5af5deef69d83241b578edd717af444acc79c
Rafaqat Ali <rafaqatmehar007@gmail.com>
2024-05-02 16:01:47 +0500

    login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,9 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Login from "./Login";
+import Button from "./Button";
+import Input from "./Input";
 
-export {Header, Footer, Container, Logo, LogoutBtn}
+
+export {Header, Footer, Container, Logo, LogoutBtn, Login, Button, Input}
