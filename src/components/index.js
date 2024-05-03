ccdb08b868cc82f33f8f58b8c731f673bb527117
Rafaqat Ali <rafaqatmehar007@gmail.com>
2024-05-03 12:31:27 +0500

    post form

--- src/components/index.js
+++ src/components/index.js
@@ -6,6 +6,7 @@ import LogoutBtn from "./Header/LogoutBtn";
 import Login from "./Login";
 import Button from "./Button";
 import Input from "./Input";
+import RTE from "./RTE";
 
 
-export {Header, Footer, Container, Logo, LogoutBtn, Login, Button, Input}
+export {Header, Footer, Container, Logo, LogoutBtn, Login, Button, Input, RTE}
